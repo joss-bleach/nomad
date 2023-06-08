00f2e26d526fa144b99ed8158fcf6a37d99a6084
Joss Bleach <jossbleach@josss-mbp.home>
2023-06-08 14:53:52 +0100

    Finished image upload component.

--- app/components/ui/forms/ImageUpload.tsx
+++ app/components/ui/forms/ImageUpload.tsx
@@ -1,11 +1,57 @@
-"use client"
-import { FC } from "react"
+"use client";
+import { FC, useCallback } from "react";
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { ImagePlus } from "lucide-react";
 
-interface ImageUploadProps {}
-
-
-const ImageUpload: FC<ImageUploadProps> = () => {
-    return <></>;
+declare global {
+  var cloudinary: any;
 }
 
-export default ImageUpload
+interface ImageUploadProps {
+  onChange: (value: string) => void;
+  value: string;
+}
+
+const ImageUpload: FC<ImageUploadProps> = ({ onChange, value }) => {
+  const handleOnUpload = useCallback(
+    (result: any) => {
+      onChange(result.info.secure_url);
+    },
+    [onChange]
+  );
+  return (
+    <CldUploadWidget
+      onUpload={handleOnUpload}
+      uploadPreset="ydtsecaw"
+      options={{
+        maxFiles: 1,
+      }}
+    >
+      {({ open }) => {
+        return (
+          <div
+            onClick={() => open?.()}
+            aria-label="Upload a photo of your property"
+            className="relative flex cursor-pointer flex-col items-center justify-center gap-4 border-2 border-dashed border-neutral-300 p-20 text-neutral-600 transition hover:opacity-70"
+          >
+            <ImagePlus size={50} />
+            <p className="text-lg font-semibold">Click to upload a photo</p>
+            {value && (
+              <div className="absolute inset-0 h-full w-full">
+                <Image
+                  alt="upload"
+                  fill
+                  style={{ objectFit: "cover" }}
+                  src={value}
+                />
+              </div>
+            )}
+          </div>
+        );
+      }}
+    </CldUploadWidget>
+  );
+};
+
+export default ImageUpload;
