f4ad05bf7eb935b278d2b08fb0a16b195615c340
Joss Bleach <jossbleach@josss-mbp.home>
2024-10-11 16:00:31 +0100

    Authenticated UI

--- src/components/navbar/user-menu-button.tsx
+++ src/components/navbar/user-menu-button.tsx
@@ -5,44 +5,88 @@ import { useAuthActions } from "@convex-dev/auth/react";
 // Hooks
 import { useModal } from "@/providers/modal-provider";
 
+// Convex
+import { useQuery } from "@tanstack/react-query";
+import { convexQuery } from "@convex-dev/react-query";
+import { api } from "../../../convex/_generated/api";
+
 // Components
 import { MenuIcon, UserIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export const UserMenuButton = () => {
-  const { onOpen } = useModal();
-  const { signOut } = useAuthActions();
   return (
     <>
       <AuthLoading>
-        <Skeleton className="h-10 w-20 rounded-full">
-          <div className="flex items-center justify-between px-3">
-            <Skeleton className="h-5 w-5 rounded" />
-            <Skeleton className="h-7 w-7 rounded-full" />
-          </div>
-        </Skeleton>
+        <UserButtonSkeleton />
       </AuthLoading>
       <Authenticated>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="rounded-full border-gray-300">
-              <MenuIcon className="mr-2 h-5 w-5" />
+        <UserButtonWithUser />
+      </Authenticated>
+      <Unauthenticated>
+        <UserButtonWithoutUser />
+      </Unauthenticated>
+    </>
+  );
+};
 
-              <Avatar className="h-7 w-7">
-                <AvatarFallback>U</AvatarFallback>
-              </Avatar>
-            </Button>
-          </DropdownMenuTrigger>
+const UserButtonWithUser = () => {
+  const { onOpen } = useModal();
+  const { signOut } = useAuthActions();
 
-          <DropdownMenuContent align="end" className="w-48">
+  const { data, isPending, error } = useQuery(
+    convexQuery(api.user.currentUser, {}),
+  );
+
+  if (isPending) {
+    return <UserButtonSkeleton />;
+  }
+
+  if (error) {
+    return null;
+  }
+
+  if (data) {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="rounded-full border-gray-300">
+            <MenuIcon className="mr-2 h-5 w-5" />
+
+            <Avatar className="h-7 w-7">
+              <AvatarFallback>{data.name?.charAt(0)}</AvatarFallback>
+              <AvatarImage src={data.image} alt={data.name} />
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuGroup>
+            <DropdownMenuItem className="cursor-pointer px-4 py-2 font-medium">
+              Trips
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer px-4 py-2 font-medium">
+              Favourites
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer px-4 py-2 font-medium">
+              Listings
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem className="cursor-pointer px-4 py-2">
+              List your home
+            </DropdownMenuItem>
             <DropdownMenuItem
               className="cursor-pointer px-4 py-2"
               role="button"
@@ -51,41 +95,58 @@ export const UserMenuButton = () => {
             >
               Sign out
             </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </Authenticated>
-      <Unauthenticated>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="rounded-full border-gray-300">
-              <MenuIcon className="mr-2 h-5 w-5" />
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  }
+};
 
-              <div className="flex size-7 items-center justify-center rounded-full bg-muted">
-                <UserIcon className="size-4 text-muted-foreground" />
-              </div>
-            </Button>
-          </DropdownMenuTrigger>
+const UserButtonWithoutUser = () => {
+  const { onOpen } = useModal();
+  const { signOut } = useAuthActions();
 
-          <DropdownMenuContent align="end" className="w-48 space-y-2">
-            <DropdownMenuItem
-              className="cursor-pointer px-4 py-2"
-              role="button"
-              aria-label="Sign up"
-              onClick={() => void onOpen("auth", { flow: "signUp" })}
-            >
-              Sign up
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              className="cursor-pointer px-4 py-2"
-              role="button"
-              aria-label="Sign up"
-              onClick={() => void onOpen("auth", { flow: "signIn" })}
-            >
-              Sign in
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </Unauthenticated>
-    </>
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="rounded-full border-gray-300">
+          <MenuIcon className="mr-2 h-5 w-5" />
+
+          <div className="flex size-7 items-center justify-center rounded-full bg-muted">
+            <UserIcon className="size-4 text-muted-foreground" />
+          </div>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" className="w-48 space-y-2">
+        <DropdownMenuItem
+          className="cursor-pointer px-4 py-2"
+          role="button"
+          aria-label="Sign up"
+          onClick={() => void onOpen("auth", { flow: "signUp" })}
+        >
+          Sign up
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="cursor-pointer px-4 py-2"
+          role="button"
+          aria-label="Sign up"
+          onClick={() => void onOpen("auth", { flow: "signIn" })}
+        >
+          Sign in
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const UserButtonSkeleton = () => {
+  return (
+    <Skeleton className="h-10 w-20 rounded-full">
+      <div className="flex items-center justify-between px-3">
+        <Skeleton className="h-5 w-5 rounded" />
+        <Skeleton className="h-7 w-7 rounded-full" />
+      </div>
+    </Skeleton>
   );
 };
